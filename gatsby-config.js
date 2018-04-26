1856f625a20f1472965f032de518ee87af554bde
Nicola Zanon <nicola.zanon86@gmail.com>
2018-04-26 22:45:29 +0200

    Add Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@ module.exports = {
     description: 'Fun and creative workshops for babies & children aged from 0 to 11'
   },
   plugins: [
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-react-next',
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
     {
