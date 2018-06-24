43a652f5d0487e7915181fd5ffc258bdfc740add
Nicola Zanon <nicola.zanon86@gmail.com>
2018-06-24 22:20:57 +0200

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://kids.lopenlab.com`,
     title_en: `Welcome to L’Open Lab for Kids!`,
     description_en: 'Fun and creative workshops for babies & children aged from 0 to 11 - Take a look at our different workshops',
     title_fr: `Bienvenue à bord de L’Open Lab for Kids !`,
-    description_fr: 'des ateliers en anglais, c réatifs et ludiques, parfaitement adaptés aux enfants de 1 à 11 ans - Nos différents ateliers'
+    description_fr: 'des ateliers en anglais, créatifs et ludiques, parfaitement adaptés aux enfants de 1 à 11 ans - Nos différents ateliers'
   },
   pathPrefix: `/`,
   plugins: [
@@ -24,6 +25,9 @@ module.exports = {
         name: 'markdown-pages',
         path: `${__dirname}/src/pages/`
       }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`
     },
     `gatsby-plugin-netlify`
   ],
