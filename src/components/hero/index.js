9d71a33bf93181124fef2122e12cc86b8fd99b1c
Nicola Zanon <nicola.zanon86@gmail.com>
2018-07-07 22:22:53 +0200

    Fix colors

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -11,7 +11,9 @@ const Hero = ({ title, subtitle, image, isHomePage, color }) => {
             {isHomePage ? (
                 <img src={image} className={style.bg} alt="L'Open Lab" />
             ) : (
-                    <img src={clip} className={style.clip} alt="L'Open Lab" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 702" preserveAspectRatio="none" height="100%" width="100%" className={style.clip}>
+                        <path d="M-23 0h1489.883c70.33 195.674-64.105 368.82-403.306 519.438C724.377 670.056 362.184 735.841-23 716.792V0z" fill={color} fillRule="evenodd" />
+                    </svg>
                 )}
             <div className="wrapper wrapper--padded">
                 <h1 className={style.hero__h1}>{title}</h1>
